ace40a432f542deacd5756117dd9715d4ead3872
Archies11 <iit2018018@iiita.ac.in>
2019-05-28 13:17:33 +0530

    first

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
   
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>hi</p>, document.getElementById('app'));
   // WAY TO GIVE DEFAULT VALS TO OPTIONS : ReactDOM.render(<IndecisionApp options={['one','two']}/>, document.getElementById('app'));
 
   
